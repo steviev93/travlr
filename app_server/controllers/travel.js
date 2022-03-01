51fd343d82a1a435b53fee381dae932ba252afaf
Stephen Valente <stephen.valente12@gmail.com>
2022-03-01 14:42:39 -0500

    Module 2 commit, added hbs files

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,7 @@
+const travel = (req, res) => {
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', {title: pageTitle});
+}
+module.exports = {
+    travel
+}
